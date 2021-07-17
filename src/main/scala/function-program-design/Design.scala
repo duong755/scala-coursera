0375d1f5712d660884cb1c563920d9cc52158255
duong755 <ngo.quang.duong.1100@gmail.com>
2021-07-17 17:03:59 +0700

    Remake main class

---
+++ src/main/scala/function-program-design/Design.scala
@@ -0,0 +1,9 @@
+package fpd
+
+import fpd.week1.{Lecture1 => Week1Lecture1}
+
+object Course2 {
+  def content: Unit = {
+    Week1Lecture1.content
+  }
+}
--- src/main/scala/function-program-design/week1/Lecture1.scala
+++ src/main/scala/function-program-design/week1/Lecture1.scala
@@ -9,7 +9,29 @@ enum JSON:
   case Null
 
 object Lecture1 {
-  def content: Unit = {}
+  def content: Unit = {
+    val data = JSON.Obj(
+      Map(
+        "firstName" -> JSON.Str("Duong"),
+        "lastName" -> JSON.Str("Ngo"),
+        "address" -> JSON.Obj(Map(
+          "street" -> JSON.Str("Kim Nguu"),
+          "city" -> JSON.Str("Ha Noi")
+        )),
+        "emails" -> JSON.Seq(List(
+          JSON.Obj(Map(
+            "type" -> JSON.Str("default"),
+            "email" -> JSON.Str("ngo.quang.duong.1100@gmail.com")
+          )),
+          JSON.Obj(Map(
+            "type" -> JSON.Str("other"),
+            "email" -> JSON.Str("duong.quang.ngo.1100@gmail.com")
+          ))
+        ))
+      )
+    )
+    println(stringify(data))
+  }
 
   def stringify(json: JSON): String = json match {
     case JSON.Bool(b) => b.toString
@@ -17,11 +39,7 @@ object Lecture1 {
     case JSON.Str(str)   => inQuotes(str)
     case JSON.Obj(bindings) =>
       bindings
-        .map(
-          { case (key, value) =>
-            s"${inQuotes(key)}: ${stringify(json)}"
-          },
-        )
+        .map((key, value) => s"${inQuotes(key)}: ${stringify(value)}")
         .mkString("{", ", ", "}")
     case JSON.Seq(elems) => elems.map(stringify).mkString("[", ", ", "]")
     case JSON.Null       => "null"
